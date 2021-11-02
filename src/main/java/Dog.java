ea712b25154992a5431d1778867e267074b1c69d
jakesheinberg <23jsheinberg@athenian.org>
2021-11-01 20:21:09 -0700

    done

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,36 @@
 public class Dog extends Pet {
     // Instance variable(s)
+    private String name;
+    private int age;
 
 
     // Constructors
-    
+    public Dog(String a, int b) {
+        super(a,b);
+    }
+
 
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Woof");
+    }
 
     // toString method
-
+    public String toString() {
+        String str = this.getAge() + ", " + this.getName();
+        return str;}
 
     // Getter
-
+public int getAge(){
+    return this.age;
+        }
+public String getName(){
+        return this.name;
+}
 
     // Setter
+    public void setDog(String name, int age) {
+        this.name = name;
+        this.age=age;
+    }
 }
